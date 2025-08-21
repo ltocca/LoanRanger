cc53939a84fe1ad6114d277dab14e447793db2fe
Leonardo Toccafondi <leonardo.toccafondi1@edu.unifi.it>
2025-08-21 19:11:33 +0200

    First Commit

---
+++ src/main/java/dev/ltocca/loanranger/LoanRangerApplication.java
@@ -0,0 +1,13 @@
+package dev.ltocca.loanranger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanRangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanRangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ltocca/loanranger/LoanRangerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ltocca.loanranger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanRangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
