d291917c38a41b71f6c9ee4b0a3892f306ca3bc6
Leonardo Toccafondi <leonardo.toccafondi1@edu.unifi.it>
2025-08-28 19:16:20 +0200

    Created the ORM package and ConnectionManager class

---
+++ docs/improvements.md
@@ -0,0 +1,4 @@
+### ConnectionManager.java
+
+- Instead of using the simple ConnectionManager implementation, maybe use Hikari to create a connection pool;
+- Use a .env file for the credentials;
---
+++ src/main/java/dev/ltocca/loanranger/ORM/ConnectionManager.java
@@ -0,0 +1,36 @@
+package dev.ltocca.loanranger.ORM;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    private static ConnectionManager instance;
+    private Connection connection;
+    private String url = "jdbc:postgresql://localhost:5432/LoanRanger_db";
+    private String username = "postgres";
+    private String password = "password";
+
+    private ConnectionManager() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Database Connection Creation Failed : " + ex.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static ConnectionManager getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new ConnectionManager();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new ConnectionManager();
+        }
+        return instance;
+    }
+}
