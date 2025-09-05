00aaf384bf006a0bde38164e75ec1f940c43cde8
Leonardo Toccafondi <leonardo.toccafondi1@edu.unifi.it>
2025-09-05 17:46:18 +0200

    added EventDAO interface

---
+++ src/main/java/dev/ltocca/loanranger/ORM/IEventDAO.java
@@ -0,0 +1,20 @@
+package dev.ltocca.loanranger.ORM;
+
+import dev.ltocca.loanranger.DomainModel.Event;
+import dev.ltocca.loanranger.DomainModel.Library;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IEventDAO {
+    Event createEvent(Event event);
+    Optional<Event> getEventById(Long id);
+    void updateEvent(Event event);
+    void deleteEvent(Long id);
+
+    void editLibrary(Event event, Library library);
+
+    List<Event> findEventsByLibrary(Library library);
+    List<Event> findUpcomingEvents();
+    List<Event> findEventsByTitle(String title);
+}
